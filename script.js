007eb0df5635d7b1a4ae8de436f5c9fc66a396fa
Bharathiselvan Rajendran <107631601+bharathiselvan451@users.noreply.github.com>
2025-09-02 18:37:37 -0600

    Updated the website.

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ window.onload = function() {
 
   let value =  fetch("${backend_api_gateway}/count",{
 
-   method : "GET",
-   mode : "cors",
-  
-
-   }).then(response=>response.json())
-   .then(data=>{ document.getElementById("replace").innerHTML = ", and this resume has been accessed "+data+" times"; console.log(data); })
+    method : "GET",
+    mode : "cors",
+  }).then(response=>response.json())
+  .then(data=>{ document.getElementById('visitorCount').textContent = data; console.log(data); console.log(data);})
 
 
    
